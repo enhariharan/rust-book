534284aefaf66299aef1c29061a2eb6a0ec7dff6
hariharan.narayanan <hariharan.narayanan@fortunapix.com>
2025-09-10 16:28:14 +0530

    feat(q-and-a): finished a basic q-and-a app server

---
+++ rust-web-development/ch-02/q-and-a/src/main.rs
@@ -0,0 +1,45 @@
+use std::io::ErrorKind;
+use std::str::FromStr;
+use warp::Filter;
+
+#[derive(Debug)]
+struct Question {
+    id: QuestionId,
+    title: String,
+    content: String,
+    tags: Option<Vec<String>>,
+}
+
+#[derive(Debug)]
+struct QuestionId(String);
+
+impl FromStr for QuestionId {
+    type Err = std::io::Error;
+
+    fn from_str(id: &str) -> Result<Self, Self::Err> {
+        match id.is_empty() {
+            false => Ok(QuestionId(id.to_string())),
+            true => Err(std::io::Error::new(
+                ErrorKind::InvalidInput,
+                "QuestionId cannot be empty!",
+            )),
+        }
+    }
+}
+
+impl Question {
+    fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
+        Question {
+            id,
+            title,
+            content,
+            tags,
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let hello = warp::get().map(|| format!("Hello, World!"));
+    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
+}
